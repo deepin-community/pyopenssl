7150e79c957d2f6909a3794798b52aa1a4818898
deepin-ci-robot <packages@deepin.org>
2026-04-17 04:48:11 +0800

    fix: update to 26.0.0 to fix CVE-2026-27448 and CVE-2026-27459
    - CVE-2026-27448: Unhandled exceptions in set_tlsext_servername_callback
      callbacks did not cancel connections (Low severity)
      Upstream: https://github.com/pyca/pyopenssl/commit/d41a814759a9fb49584ca8ab3f7295de49a85aa0
    
    - CVE-2026-27459: Buffer overflow if DTLS cookie callback returned too
      long of a value (Medium severity)
      Upstream: https://github.com/pyca/pyopenssl/commit/57f09bb4bb051d3bc2a1abd36e9525313d5cd408
    
    New upstream version 26.0.0 includes fixes for both CVEs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,33 @@
 # Copyright (c) The pyOpenSSL developers
 # See LICENSE for details.
 
+import pathlib
 from tempfile import mktemp
 
 import pytest
 
+from OpenSSL.SSL import OPENSSL_VERSION, SSLeay_version
 
-def pytest_report_header(config):
-    import OpenSSL.SSL
+is_awslc = b"AWS-LC" in SSLeay_version(OPENSSL_VERSION)
+
+
+def pytest_report_header(config: pytest.Config) -> str:
     import cryptography
 
-    return "OpenSSL: {openssl}\ncryptography: {cryptography}".format(
-        openssl=OpenSSL.SSL.SSLeay_version(OpenSSL.SSL.SSLEAY_VERSION),
-        cryptography=cryptography.__version__,
+    import OpenSSL.SSL
+
+    return (
+        f"OpenSSL: "
+        f"{OpenSSL.SSL.SSLeay_version(OpenSSL.SSL.SSLEAY_VERSION)!r}\n"
+        f"cryptography: {cryptography.__version__}"
     )
 
 
 @pytest.fixture
-def tmpfile(tmpdir):
+def tmpfile(tmp_path: pathlib.Path) -> bytes:
     """
     Return UTF-8-encoded bytes of a path to a tmp file.
 
     The file will be cleaned up after the test run.
     """
-    return mktemp(dir=tmpdir.dirname).encode("utf-8")
+    return mktemp(dir=tmp_path).encode("utf-8")
